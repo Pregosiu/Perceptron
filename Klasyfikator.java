3f9e420aeca0665463042e748e459563e2d54a19
Pregosiu <97323827+Pregosiu@users.noreply.github.com>
2023-06-13 23:29:11 +0200

    Update Klasyfikator.java

--- Klasyfikator.java
+++ Klasyfikator.java
@@ -29,11 +29,6 @@ public class Klasyfikator {
         Double g;
         String dec = "";
         String dec2 = "";
-//        ArrayList<SimpleImmutableEntry<String,ArrayList<Double>>> nazwy = trainset.stream()
-//                .map(x -> x.getKey())
-//                .distinct()
-//
-//                .collect(Collectors.toCollection(ArrayList::new));
 
         List<String> nazwy = trainset.stream()
                 .map(x -> x.getKey())
@@ -165,43 +160,4 @@ public class Klasyfikator {
 
 
     }
-
-    //Do proj1
-
-//    public static Double liczEuklides(ArrayList<Double> wektor1, ArrayList<Double> wektor2){
-//        Double wynik = 0.0;
-//        for (int i = 0; i < wektor1.size(); i++) {
-//            wynik = wynik + Math.pow(wektor1.get(i)-wektor2.get(i),2);
-//        }
-//        return wynik;
-//    }
-
-//    public static Map<Object, Long> klasyfikuj(ArrayList<SimpleImmutableEntry<String,ArrayList<Double>>> trainset, ArrayList<ArrayList<Double>> testset, int k){
-//        Map<Object, Long> aa;
-//        aa = testset.stream()
-//                .map(x -> trainset.stream()
-//                        .map(s -> new SimpleImmutableEntry(s.getKey(),liczEuklides(x,s.getValue())))
-//                        .sorted((o1,o2) -> {
-//                            if((Double)(o1.getValue()) > (Double) (o2.getValue())){
-//                                return 1;
-//                            }
-//                            if((Double)(o2.getValue()) > (Double) (o1.getValue())){
-//                                return -1;
-//                            }
-//                            if((o1.getValue()) == (o2.getValue())){
-//                                return 1;
-//                            }
-//                            return 1;
-//                        })
-//                        .limit(k)
-//                        .collect(Collectors.toCollection(ArrayList<SimpleImmutableEntry>::new)))
-//                .map(x-> x.stream()
-//                        .collect(Collectors.groupingBy(s -> s.getKey(), Collectors.counting())))
-//                .map(x-> x.entrySet().stream()
-//                        .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-//                        .limit(1)
-//                        .collect(Collectors.toMap(s -> s.getKey(), a -> a.getValue())))
-//                .collect(Collectors.groupingBy(x-> x.keySet(), Collectors.counting()));
-//        return aa;
-//    }
 }
